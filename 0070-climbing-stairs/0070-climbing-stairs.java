615c4e103ae41d7637a0ede8bde1a69ece2e6627
Priyanka Kumari <2300033474@kluniversity.in>
2026-07-23 15:21:31 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.62%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+       int solve(int n,int dp[]){
+        if(n==1||n==2){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+       }
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+}
